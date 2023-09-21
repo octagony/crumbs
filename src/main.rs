244919872d78da61ad868fa4f300dd4100293a38
octagony <moctagony@gmail.com>
2023-09-21 20:44:31 +0300

    [FEAT] Setup basic logic of displaying elements

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
     path::{self, PathBuf},
 };
 
-use fltk::{prelude::*, *};
+use fltk::{prelude::*, *, input::{Input, FloatInput}, text::{TextDisplay, TextBuffer}};
 
 fn read_dir() -> Vec<String> {
     let paths = match fs::read_dir("/usr/bin") {
@@ -22,26 +22,36 @@ fn read_dir() -> Vec<String> {
 
 fn main() {
     let app = app::App::default();
-    // To load a font by path, check the App::load_font() method
     let binaries = read_dir();
-    // println!("{:?}", fonts);
-    let mut wind = window::Window::default().with_size(400, 300);
-    let mut frame = frame::Frame::default().size_of(&wind);
-    frame.set_label_size(30);
-    wind.set_color(enums::Color::White);
+    
+    let mut wind = window::Window::default().with_size(400, 300).center_screen().with_label("Crumbs");
+    
+
+    let mut disp = TextDisplay::new(5, 5, 390, 250, None);
+
+    let mut inp1 = FloatInput::new(0,0,50,50,"").center_x(&wind);
+
+    let mut buffer = TextBuffer::default();
+    for bin in binaries.iter(){
+        buffer.append(&format!("{}\n", bin));
+        disp.set_buffer(buffer.clone())
+    }
+    ;
+
     wind.end();
     wind.show();
-    println!(
-        "The system has {} binaries!\nStarting slideshow!",
-        binaries.len()
-    );
-    let mut i = 0;
-    while app.wait() {
-        if i == binaries.len() {
-            i = 0;
-        }
-        frame.set_label(&format!("[{}]", binaries[i]));
-        app::sleep(0.5);
-        i += 1;
-    }
+
+    
+    app.run().unwrap();
+    
+    // let mut i = 0;
+    // while app.wait() {
+    //     if i == binaries.len() {
+    //         i = 0;
+    //     }
+    //     frame.set_label_color(enums::Color::White);
+    //     frame.set_label(&format!("[{}]", binaries[i]));
+    //     app::sleep(0.5);
+    //     i += 1;
+    // }
 }
