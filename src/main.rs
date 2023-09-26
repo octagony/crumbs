9043f48b739918118a2a260f4f9ecb30d2d74b59
octagony <moctagony@gmail.com>
2023-09-26 22:12:23 +0300

    [FEAT] Added fltk-flex and fltk-theme crates

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,15 @@ use std::{
     path::{self, PathBuf},
 };
 
-use fltk::{prelude::*, *, input::{Input, FloatInput}, text::{TextDisplay, TextBuffer}};
+use fltk::{
+    enums::Color,
+    input::{FloatInput, Input},
+    prelude::*,
+    text::{TextBuffer, TextDisplay},
+    *,
+};
+
+use fltk_theme::{ThemeType, WidgetTheme};
 
 fn read_dir() -> Vec<String> {
     let paths = match fs::read_dir("/usr/bin") {
@@ -21,29 +29,46 @@ fn read_dir() -> Vec<String> {
 }
 
 fn main() {
-    let app = app::App::default();
+    let app = app::App::default().with_scheme(app::Scheme::Gtk);
+
     let binaries = read_dir();
-    
-    let mut wind = window::Window::default().with_size(400, 300).center_screen().with_label("Crumbs");
-    
 
-    let mut disp = TextDisplay::new(5, 5, 390, 250, None);
+    let mut wind = window::Window::default()
+        .with_size(400, 300)
+        .with_label("Crumbs")
+        .with_size(400, 300)
+        .with_label("Crumbs");
 
-    let mut inp1 = FloatInput::new(0,0,50,50,"").center_x(&wind);
+    let mut disp = TextDisplay::default().with_size(1700, 100);
 
-    let mut buffer = TextBuffer::default();
-    for bin in binaries.iter(){
-        buffer.append(&format!("{}\n", bin));
-        disp.set_buffer(buffer.clone())
-    }
-    ;
+    let mut inp1 = FloatInput::new(0, 0, 50, 50, "").center_y(&wind);
+
+    let mut btn = button::Button::default()
+        .with_size(80, 30)
+        .center_of_parent()
+        .with_label("Click");
+    theme_button(&mut btn);
 
     wind.end();
     wind.show();
 
-    
+    btn.set_callback(move |b| {
+        b.set_label("Clicked");
+        wind.set_label("Clicked")
+    });
+
+    let mut buffer = TextBuffer::default();
+    for bin in binaries.iter() {
+        buffer.append(&format!("{}\n", bin));
+        disp.set_buffer(buffer.clone())
+    }
+
     app.run().unwrap();
-    
+
+    // Helper function for find binary
+    // let elems: Vec<String> = binaries.into_iter().filter(|e| e.contains("kin")).collect();
+    // println!("{:?}", elems);
+    //
     // let mut i = 0;
     // while app.wait() {
     //     if i == binaries.len() {
@@ -53,5 +78,9 @@ fn main() {
     //     frame.set_label(&format!("[{}]", binaries[i]));
     //     app::sleep(0.5);
     //     i += 1;
-    // }
+    //}
+}
+
+fn theme_button(btn: &mut button::Button) {
+    btn.set_color(Color::from_rgb(255, 0, 0).darker());
 }
